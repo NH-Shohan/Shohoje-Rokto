2cf0937cfcc6bccebe05a0b1aa3141ca7b486974
Nahim Hossain Shohan <nh.shohan6@gmail.com>
2024-02-25 22:13:48 +0600

    donor search all features added

--- src/app/(Donor_Search)/DonorCard.jsx
+++ src/app/(Donor_Search)/DonorCard.jsx
@@ -20,6 +20,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { PiSealCheckDuotone } from "react-icons/pi";
+import noData from "../../../public/assets/noData.svg";
 
 const DonorCard = ({ donors }) => {
   const postsPerPage = 16;
@@ -36,6 +37,12 @@ const DonorCard = ({ donors }) => {
     if (storedPage) {
       setCurrentPage(parseInt(storedPage));
     }
+
+    const timeoutId = setTimeout(() => {
+      localStorage.removeItem("currentPage");
+    }, 5 * 60 * 1000);
+
+    return () => clearTimeout(timeoutId);
   }, []);
 
   useEffect(() => {
@@ -59,6 +66,7 @@ const DonorCard = ({ donors }) => {
         );
       });
       setFilteredDonors(filteredData);
+      setCurrentPage(1);
     } else {
       setFilteredDonors(donors);
     }
@@ -95,178 +103,193 @@ const DonorCard = ({ donors }) => {
 
   return (
     <>
-      {isClient ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-10">
-          {currentItems.map(
-            (
-              {
-                image,
-                name,
-                bloodGroup,
-                availability,
-                city,
-                gender,
-                age,
-                type,
-              },
-              index
-            ) => (
-              <div
-                key={index}
-                className="relative group block p-2 h-full w-full"
-                onMouseEnter={() => setHoveredIndex(index)}
-                onMouseLeave={() => setHoveredIndex(null)}
-              >
-                <AnimatePresence>
-                  {hoveredIndex === index && (
-                    <motion.span
-                      className="absolute inset-0 h-full w-full bg-light dark:bg-muted block rounded-3xl -z-10"
-                      layoutId="hoverBackground"
-                      initial={{ opacity: 0 }}
-                      animate={{
-                        opacity: 1,
-                        transition: { duration: 0.15 },
-                      }}
-                      exit={{
-                        opacity: 0,
-                        transition: { duration: 0.15, delay: 0.2 },
-                      }}
-                    />
-                  )}
-                </AnimatePresence>
-                <div className="border bg-white rounded-2xl p-6 flex flex-col gap-3 transition dark:bg-secondary relative group">
-                  <Image
-                    src={image}
-                    alt="user image"
-                    width={120}
-                    height={120}
-                    className="mx-auto capitalize"
-                    priority
-                  />
-                  <p className="w-full flex gap-2 justify-center items-center">
-                    {name}
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger>
-                          <PiSealCheckDuotone
-                            className={
-                              type === "Certified" || type === "certified"
-                                ? "text-green-600"
-                                : type === "Verified" || type === "verified"
-                                ? "text-blue-500"
-                                : "text-primary"
-                            }
-                          />
-                        </TooltipTrigger>
-                        <TooltipContent className="flex flex-col gap-1">
-                          <p className="flex items-center gap-1 text-green-600">
-                            <PiSealCheckDuotone />
-                            Certified
-                          </p>
-                          <p className="flex items-center gap-1 text-blue-500">
-                            <PiSealCheckDuotone />
-                            Verified
-                          </p>
-                          <p className="flex items-center gap-1 text-primary">
-                            <PiSealCheckDuotone />
-                            Normal
-                          </p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  </p>
-                  <div className="flex gap-5">
-                    <p className="bg-light rounded-xl flex justify-center items-center w-1/2 text-[48px] text-primary dark:text-white uppercase font-medium">
-                      {bloodGroup}
-                    </p>
-                    <div className="capitalize">
-                      <li
-                        className={
-                          availability.toLowerCase() === "available"
-                            ? "text-green-600"
-                            : "text-primary"
-                        }
-                      >
-                        {availability}
-                      </li>
-                      <li>{city}</li>
-                      <li>{gender}</li>
-                      <li>{age}</li>
-                    </div>
-                  </div>
-                  <Button variant="outline">See Details</Button>
-                </div>
-              </div>
-            )
-          )}
+      {filteredDonors.length === 0 ? (
+        <div className="flex flex-col justify-center items-center h-[calc(100vh-174px)] w-full relative">
+          <h1 className="font-light text-primary absolute top-[10%]">
+            No Donors Found 😔
+          </h1>
+          <p className>Sorry 😞</p>
+          <div className="relative w-[100%] h-[80%]">
+            <Image src={noData} alt="No data Found Image" fill />
+          </div>
         </div>
       ) : (
-        <div className="grid grid-cols-4 mt-10"></div>
-      )}
+        <>
+          {isClient ? (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-10">
+              {currentItems.map(
+                (
+                  {
+                    image,
+                    name,
+                    bloodGroup,
+                    availability,
+                    city,
+                    gender,
+                    age,
+                    type,
+                  },
+                  index
+                ) => (
+                  <div
+                    key={index}
+                    className="relative group block p-2 h-full w-full"
+                    onMouseEnter={() => setHoveredIndex(index)}
+                    onMouseLeave={() => setHoveredIndex(null)}
+                  >
+                    <AnimatePresence>
+                      {hoveredIndex === index && (
+                        <motion.span
+                          className="absolute inset-0 h-full w-full bg-light dark:bg-muted block rounded-3xl -z-10"
+                          layoutId="hoverBackground"
+                          initial={{ opacity: 0 }}
+                          animate={{
+                            opacity: 1,
+                            transition: { duration: 0.15 },
+                          }}
+                          exit={{
+                            opacity: 0,
+                            transition: { duration: 0.15, delay: 0.2 },
+                          }}
+                        />
+                      )}
+                    </AnimatePresence>
+                    <div className="border bg-white rounded-2xl p-6 flex flex-col gap-3 transition dark:bg-secondary relative group">
+                      <Image
+                        src={image}
+                        alt="user image"
+                        width={120}
+                        height={120}
+                        className="mx-auto capitalize"
+                        priority
+                      />
+                      <p className="w-full flex gap-2 justify-center items-center">
+                        {name}
+                        <TooltipProvider>
+                          <Tooltip>
+                            <TooltipTrigger>
+                              <PiSealCheckDuotone
+                                className={
+                                  type === "Certified" || type === "certified"
+                                    ? "text-green-600"
+                                    : type === "Verified" || type === "verified"
+                                    ? "text-blue-500"
+                                    : "text-primary"
+                                }
+                              />
+                            </TooltipTrigger>
+                            <TooltipContent className="flex flex-col gap-1">
+                              <p className="flex items-center gap-1 text-green-600">
+                                <PiSealCheckDuotone />
+                                Certified
+                              </p>
+                              <p className="flex items-center gap-1 text-blue-500">
+                                <PiSealCheckDuotone />
+                                Verified
+                              </p>
+                              <p className="flex items-center gap-1 text-primary">
+                                <PiSealCheckDuotone />
+                                Normal
+                              </p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </p>
+                      <div className="flex gap-5">
+                        <p className="bg-light rounded-xl flex justify-center items-center w-1/2 text-[48px] text-primary dark:text-white uppercase font-medium">
+                          {bloodGroup}
+                        </p>
+                        <div className="capitalize">
+                          <li
+                            className={
+                              availability.toLowerCase() === "available"
+                                ? "text-green-600"
+                                : "text-primary"
+                            }
+                          >
+                            {availability}
+                          </li>
+                          <li>{city}</li>
+                          <li>{gender}</li>
+                          <li>{age}</li>
+                        </div>
+                      </div>
+                      <Button variant="outline">See Details</Button>
+                    </div>
+                  </div>
+                )
+              )}
+            </div>
+          ) : (
+            <div className="grid grid-cols-4 mt-10"></div>
+          )}
 
-      <div className="my-10 w-full">
-        <Pagination>
-          <PaginationContent>
-            {
-              <PaginationItem className="cursor-pointer">
-                <PaginationPrevious
-                  onClick={() => setCurrentPage(currentPage - 1)}
-                  disabled={currentPage === 1}
-                />
-              </PaginationItem>
-            }
-
-            {Array.from({
-              length: Math.ceil(filteredDonors.length / postsPerPage),
-            }).map((_, index) => {
-              const pageNumber = index + 1;
-              const isActive = pageNumber === currentPage;
-              if (
-                pageNumber === 1 ||
-                pageNumber === currentPage - 2 ||
-                pageNumber === currentPage - 1 ||
-                pageNumber === currentPage ||
-                pageNumber === currentPage + 1 ||
-                pageNumber === currentPage + 2 ||
-                pageNumber === Math.ceil(filteredDonors.length / postsPerPage)
-              ) {
-                return (
-                  <PaginationItem key={index} className="cursor-pointer">
-                    <PaginationLink
-                      onClick={() => paginate(pageNumber)}
-                      isActive={isActive}
-                    >
-                      {pageNumber}
-                    </PaginationLink>
+          <div className="my-10 w-full">
+            <Pagination>
+              <PaginationContent>
+                {
+                  <PaginationItem className="cursor-pointer">
+                    <PaginationPrevious
+                      onClick={() => setCurrentPage(currentPage - 1)}
+                      disabled={currentPage === 1}
+                    />
                   </PaginationItem>
-                );
-              } else if (
-                pageNumber === currentPage - 3 ||
-                pageNumber === currentPage + 3
-              ) {
-                return (
-                  <PaginationItem key={index}>
-                    <PaginationEllipsis />
-                  </PaginationItem>
-                );
-              }
-              return null;
-            })}
+                }
 
-            {
-              <PaginationItem className="cursor-pointer">
-                <PaginationNext
-                  onClick={() => setCurrentPage(currentPage + 1)}
-                  disabled={
-                    currentPage ===
-                    Math.ceil(filteredDonors.length / postsPerPage)
+                {Array.from({
+                  length: Math.ceil(filteredDonors.length / postsPerPage),
+                }).map((_, index) => {
+                  const pageNumber = index + 1;
+                  const isActive = pageNumber === currentPage;
+                  if (
+                    pageNumber === 1 ||
+                    pageNumber === currentPage - 2 ||
+                    pageNumber === currentPage - 1 ||
+                    pageNumber === currentPage ||
+                    pageNumber === currentPage + 1 ||
+                    pageNumber === currentPage + 2 ||
+                    pageNumber ===
+                      Math.ceil(filteredDonors.length / postsPerPage)
+                  ) {
+                    return (
+                      <PaginationItem key={index} className="cursor-pointer">
+                        <PaginationLink
+                          onClick={() => paginate(pageNumber)}
+                          isActive={isActive}
+                        >
+                          {pageNumber}
+                        </PaginationLink>
+                      </PaginationItem>
+                    );
+                  } else if (
+                    pageNumber === currentPage - 3 ||
+                    pageNumber === currentPage + 3
+                  ) {
+                    return (
+                      <PaginationItem key={index}>
+                        <PaginationEllipsis />
+                      </PaginationItem>
+                    );
                   }
-                />
-              </PaginationItem>
-            }
-          </PaginationContent>
-        </Pagination>
-      </div>
+                  return null;
+                })}
+
+                {
+                  <PaginationItem className="cursor-pointer">
+                    <PaginationNext
+                      onClick={() => setCurrentPage(currentPage + 1)}
+                      disabled={
+                        currentPage ===
+                        Math.ceil(filteredDonors.length / postsPerPage)
+                      }
+                    />
+                  </PaginationItem>
+                }
+              </PaginationContent>
+            </Pagination>
+          </div>
+        </>
+      )}
     </>
   );
 };
