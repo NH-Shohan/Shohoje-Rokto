2b4860f6468cf8f42e0370e7b48af9e475b62850
Nahim Hossain Shohan <nh.shohan6@gmail.com>
2024-03-05 23:58:07 +0600

    Be_Donor: Medical info completed

---
+++ src/context/BeDonorContext.js
@@ -0,0 +1,33 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+const BeDonorContext = createContext();
+
+export const BeDonorProvider = ({ children }) => {
+  const [donorData, setDonorData] = useState({
+    name: "",
+    email: "",
+    bloodGroup: "",
+    dateOfBirth: "",
+    gender: "",
+    phoneNumber: "",
+    optionalPhoneNumber: "",
+    division: "",
+    district: "",
+    subdistrict: "",
+    donatedBefore: "",
+    donationDate: "",
+    maritalStatus: "",
+    socialMediaLink: "",
+    bio: "",
+    contectMethod: "",
+  });
+
+  return (
+    <BeDonorContext.Provider value={{ donorData, setDonorData }}>
+      {children}
+    </BeDonorContext.Provider>
+  );
+};
+
+export const useBeDonor = () => useContext(BeDonorContext);
