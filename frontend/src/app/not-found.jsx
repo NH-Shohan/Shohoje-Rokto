8aa399aaa41add536adce320c61ff31d7d8afb59
Nahim Hossain Shohan <nh.shohan6@gmail.com>
2024-03-28 20:42:40 +0600

    Not_Found: UI changed

--- frontend/src/app/not-found.jsx
+++ frontend/src/app/not-found.jsx
@@ -1,24 +1,41 @@
 "use client";
-import { Button } from "@/components/ui/button";
+
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function NotFound() {
   const router = useRouter();
+  const [countdown, setCountdown] = useState(4);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown((prevCount) => prevCount - 1);
+    }, 1000);
+
+    setTimeout(() => {
+      router.back();
+    }, 4000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="w-2/3">
-        <Image
-          src={"/assets/404.svg"}
-          alt="404 image"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "100%", height: "auto" }}
-          priority
-        />
+    <div className="grid items-center justify-center h-[calc(100vh-60px)]">
+      <Image
+        src={"/assets/404.svg"}
+        alt="404 image"
+        width={0}
+        height={0}
+        style={{ width: "80vh", height: "auto" }}
+        priority
+      />
+      <div className="-mt-60">
+        <p className="text-xl text-center text-foreground">Going Back In</p>
+        <p className="text-center text-9xl text-primary font-bold">
+          {countdown}
+        </p>
       </div>
-      <Button onClick={() => router.back()}>Go Back</Button>
     </div>
   );
 }
