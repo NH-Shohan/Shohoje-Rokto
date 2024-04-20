83e9eb0930f6c1ad5b9ddeb2ea3e4e70ea572888
Nahim Hossain Shohan <nh.shohan6@gmail.com>
2024-04-20 17:43:21 +0600

    Monorepo: fixing production

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,22 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../..', 'frontend', '.next'),
-      exclude: ['/api/*'],
-    }),
-    AuthModule,
-    UsersModule,
-    ConfigModule.forRoot(),
-  ],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
